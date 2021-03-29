373efb8e92f60f64b509934c5130d01ce984e6a0
Your Lord and Master <kuriosly@gmail.com>
2021-03-29 14:30:43 -0700

    fixing maps

--- maps.js
+++ maps.js
@@ -2,4 +2,4 @@ const mapData = require('./raw/maps.json')
 
 const allMapNames = [...new Set([...mapData.byPlayerSize['2'], ...mapData.byPlayerSize['4'], ...mapData.byPlayerSize['6'], ...mapData.byPlayerSize['8']])]
 
-module.exports = [mapData, allMapNames]
+module.exports = { mapData: mapData, allMapNames: allMapNames }
