9d849e341845d2bd25357aeb79ad680afe26fa91
ReeF <vesecky.lukas90@gmail.com>
2025-09-27 21:23:08 +0200

    added conflict test

--- test/index.test.js
+++ test/index.test.js
@@ -16,3 +16,23 @@ describe('Divisions', () => {
     expect(data.divisions.divisionsById[431].name).to.equal('97-ya Gvard. Strelkovy')
   })
 })
+
+
+describe('No conflicts', () => {
+
+  it('no_conflicting_ids?', () => {
+
+    const divs = [...data.divisions.divisionsAllies, ...data.divisions.divisionsAxis,
+    ...data.divisions.divisionsPact, ...data.divisions.divisionsNato];
+
+    while (divs.length !== 0) {
+
+      const divId = divs.pop().id;
+
+      divs.forEach(d => {
+        expect(divId).is.not.equal(d.id);
+      });
+    }
+  })
+
+});
