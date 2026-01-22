aa75a6564703f17689936614e64dcc017c7161b1
ReeF <vesecky.lukas90@gmail.com>
2026-01-22 18:42:11 +0100

    npm build

---
+++ dist/index.test-d.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tsd_1 = require("tsd");
+const Data = require("./index");
+(0, tsd_1.expectType)(Data.maps.allMapNames);
+(0, tsd_1.expectType)(Data.maps);
+(0, tsd_1.expectType)(Data.misc.mode);
+(0, tsd_1.expectType)(Data.divisions.divisionsAllies);
+//# sourceMappingURL=index.test-d.js.map
